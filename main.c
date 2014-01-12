fb5007f9d2c15ad1dba704b9145139f997339485
JLD <JLD@JLD-PC>
2014-01-12 01:50:18 +0100

    optimalizacia

--- main.c
+++ main.c
@@ -4,7 +4,7 @@
  *
  * Created on Utorok, 2013, november 5, 19:32
  */
-
+    
 #include <stdio.h>
 #include <stdlib.h>
 #include"challenge.h"
@@ -16,37 +16,39 @@ int main() {
     int i,j,n,g=1;
     char **c;
     char **b;
+    INPUT *p;
     
-    n=input();
-    b=generatorik(n);
+    p=input();
+    b=generatorik(p);
+    getchar();
     FILE *fw;
     fw=fopen("data.txt", "w");
     
     printf( "\n 0. generation");
-        for(i=0; i<n; i++){
+        for(i=0; i<p->count; i++){
             printf("\n");
-            for(j=0; j<5; j++){
+            for(j=0; j<=p->number; j++){
                 printf("%c", b[i][j]);}}
-    
     while(1){
         
-        for(i=0; i<n; i++){
-            if(b[i][4]>40){
+        for(i=0; i<p->count; i++){
+            if(b[i][p->number]>100){
                 printf("done");
                 getchar();
                 return (EXIT_SUCCESS);}}
         
-        c=natural_selection(n,b);
-        b=natural_selection_time_to_die(n,b,c);
+        c=natural_selection(p,b);
+        b=natural_selection_time_to_die(p,b,c);
         
         fprintf(fw, "\n %d. generation",g);
-        for(i=0; i<n; i++){
+        for(i=0; i<p->count; i++){
             fprintf(fw, "\n");
-            for(j=0; j<5; j++){
-                fprintf(fw,"%c", b[i][j]);}}
+            for(j=0; j<=p->number; j++){
+                fprintf(fw, "%c", b[i][j]);}}
     g++;}
     getchar();
     getchar();
     return (EXIT_SUCCESS);
+
 }
 
