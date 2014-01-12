1107bd824f4062dd43a580ce55f22b4f11c7a55c
JLD <JLD@JLD-PC>
2014-01-12 01:51:03 +0100

    optimalizacia

--- newfile.c
+++ newfile.c
@@ -5,18 +5,18 @@
 #include"challenge.h"
 
 
-char **generatorik(int n){
+char **generatorik(INPUT *p){
     int i,j,c;
     char **b;
     srand(time(NULL));
     
-    b=(char**)malloc(n*sizeof(char*));
-    for(i=0; i<n; i++){
-        b[i]=(char*)malloc(5*sizeof(char));}
+    b=(char**)malloc(p->count*sizeof(char*));
+    for(i=0; i<p->count; i++){
+        b[i]=(char*)malloc((p->number+1)*sizeof(char));}
     
-    for(i=0; i<n; i++){
-        for(j=0; j<5; j++){
-           if(j<4){ 
+    for(i=0; i<p->count; i++){
+        for(j=0; j<p->number; j++){
+           if(j<p->number){ 
             c=(rand()%26)+1;
             switch(c){
                 case 1: b[i][j]='a';
@@ -98,87 +98,92 @@ char **generatorik(int n){
                 break;}}
            else b[i][j]=0;}}
     
-    fitness(n, b);}
+    fitness(p, b);}
 
-char **fitness(int n, char** population){
+char **fitness(INPUT *p, char** population){
+
     int i,j,a,v=0,g=0;
-    char goal[]="pero";
-    char protogoal[4];
+    char *protogoal;
+    protogoal=(char*)malloc(p->number*sizeof(char));
     
-    for(i=0; i<n; i++){
+    for(i=0; i<p->count; i++){
         
-        for(j=0; j<4; j++){
-            protogoal[j]=goal[j];}
+        for(j=0; j<p->number; j++){
+            protogoal[j]=p->word[j];}
         
-        for(j=0; j<4; j++){
-            for(a=0; a<4; a++){
+        for(j=0; j<p->number; j++){
+            for(a=0; a<p->number; a++){
                 if(population[i][j]==protogoal[a]){
                     protogoal[a]='1';
                     v++;}}}
         
-        population[i][4]=v;
+        population[i][p->number]=v;
         v=0;}
     
-    for(i=0; i<n; i++){
-        for(j=0; j<4; j++){
-            if(population[i][j]==goal[j]){
+    for(i=0; i<p->count; i++){
+        g=0;
+        for(j=0; j<p->number; j++){
+            if(population[i][j]==p->word[j]){
                 g++;}}
-        switch(g){
-            case 1: population[i][4]+=15;
-                    g=0;
-                    break;
-            case 2: population[i][4]+=25;
-                    g=0;
-                    break;
-            case 3: population[i][4]+=35;
-                    g=0;
-                    break;
-            case 4: population[i][4]+=45;
-                    g=0;
-                    break;
-            default: break;}}
+                if(g<p->number)
+                {
+                population[i][p->number]+=g*3;
+                }
+                else
+                {
+                population[i][p->number]+=100;
+                break;
+                }
+    }
     
     return population;}
 
-char **natural_selection(int n, char** parents){
+
+char **natural_selection(INPUT *p, char** parents){
     srand(time(NULL));
     char **childrens;
     int i=0,j,choose=0,mother,father,s;
     
-    childrens=(char**)malloc(n*sizeof(char*));
-    for(j=0; j<n; j++){
-        childrens[j]=(char*)malloc(5*sizeof(char));}
+    childrens=(char**)malloc(p->count*sizeof(char*));
+    for(j=0; j<p->count; j++){
+        childrens[j]=(char*)malloc((p->number+1)*sizeof(char));}
    
-   for(j=0; j<n; j++){
+   for(j=0; j<p->count; j++){
     
-    while(1){
-        s=(rand()%n);
+     while(1){
+        s=(rand()%p->count);
         choose=(rand()%10)+1;
-        if(parents[s][4]>=choose){
+        if(parents[s][p->number]>=choose){
             mother=s;
             break;}}
     
-    while(1){
-        s=(rand()%n);
+     while(1){
+        s=(rand()%p->count);
         choose=(rand()%10)+1;
-        if(parents[s][4]>=choose && s!=mother){
+        if(parents[s][p->number]>=choose && s!=mother){
             father=s;
-            choose=100;
             break;}}
     
-    childrens[j][0]=parents[mother][0];
-    childrens[j][1]=parents[mother][1];
-    childrens[j][2]=parents[father][2];
-    childrens[j][3]=parents[father][3];
-    childrens[j][4]=0;}
+     for(i=0; i<p->number; i++)
+     {
+         if(i%2==0)
+         {
+             childrens[j][i]=parents[mother][i];
+         }
+         else
+         {
+             childrens[j][i]=parents[father][i];
+         }
+     }
+    childrens[j][p->number]=0;}
     
-    mutation(n, childrens);}
+    mutation(p, childrens);}
 
-char **mutation(int n, char **childrens){
+char **mutation(INPUT *p, char **childrens){
     int i,j,m1,m2;
     srand(time(NULL));
-    for(i=0; i<n; i++){
-        m1=(rand()%4);
+    for(i=0; i<p->count; i++){
+        m1=(rand()%p->number);
         m2=(rand()%26)+1;
         
         switch(m2){
@@ -260,56 +265,83 @@ char **mutation(int n, char **childrens){
                 case 26: childrens[i][m1]='z';
                 break;}}
     
-    fitness(n, childrens);}
+    fitness(p, childrens);}
 
-char **natural_selection_time_to_die(int n, char **origins, char **newcomers){
+char **natural_selection_time_to_die(INPUT *p, char **origins, char **newcomers){
     char **new_population;
     int i,choose,s,d,j;
     srand(time(NULL));
     
-    new_population=(char**)malloc(n*sizeof(char*));
-    for(i=0; i<n; i++){
-        new_population[i]=(char*)malloc(5*sizeof(char));}
+    new_population=(char**)malloc(p->count*sizeof(char*));
+    for(i=0; i<p->count; i++){
+        new_population[i]=(char*)malloc((p->number+1)*sizeof(char));}
 
-    for(i=0; i<n; i++){
+    for(i=0; i<p->count; i++){
         
         while(1){
             choose=(rand()%15)+1;
-            d=(rand()%n);
-            s=(rand()%n);
-            if(origins[s][4]>=choose || newcomers[d][4]>=choose){
+            d=(rand()%p->count);
+            s=(rand()%p->count);
+            if(origins[s][p->number]>=choose || newcomers[d][p->number]>=choose){
                 
-                if(origins[s][4]>newcomers[d][4]){
-                    for(j=0; j<5; j++){
+                if(origins[s][p->number]>newcomers[d][p->number]){
+                    for(j=0; j<=p->number; j++){
                         new_population[i][j]=origins[s][j];}
                     break;}
                 
-                else if(origins[s][4]==newcomers[d][4]){
-                     for(j=0; j<5; j++){
+                else if(origins[s][p->number]==newcomers[d][p->number]){
+                     for(j=0; j<=p->number; j++){
                         new_population[i][j]=origins[s][j];}
                      break;}
              
                 else{
-                    for(j=0; j<5; j++){
+                    for(j=0; j<=p->number; j++){
                         new_population[i][j]=newcomers[d][j];}
                     break;}
                 }}}
     
     return new_population;}
 
-int input(void){
-    int n;
+
+
+INPUT *input(void)
+{
+    char *word;
+    int i=0,n;
+    INPUT *p;
+    p=(INPUT*)malloc(sizeof(INPUT));
     
     printf("Zadaj pocet jedincov:\n");
     scanf("%d", &n);
     getchar();
-    if((n<10) && (n>100)){
+    if((n<10) && (n>100))
+    {
         system("cls");
         printf("Nevhodny parameter, opakuj");
         getchar();
-        system("cls");
-        n=input();}
+        exit (EXIT_FAILURE); 
+    }
+    p->count=n;
     
-    return n;}
+    word=(char*)malloc(23*sizeof(char));
+    printf("Zadaj slovo: \n");
+    while(1)
+    {
+       scanf("%c", &word[i]);
+       if(word[i]=='\n')
+       {
+           break;
+       }
+       if(i>22)
+       {
+         exit (EXIT_FAILURE);  
+       }
+       i++;
+    }
+    printf("\n");
+    p->number=i;
+    p->word=word;
+    return p;
+ }
 
 
